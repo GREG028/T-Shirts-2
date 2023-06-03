0a53146e3421f63d1a5f8f7edbccc2a3ea395c23
Grzegorz Diaconescu <grzes.diac@gmail.com>
2023-06-03 14:47:14 +0200

    Created new files and modified server.js so it has connections with those files

---
+++ routes/designers.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+
+// All Designers Route
+router.get('/', (req, res) => {
+    res.render('designers/index')
+})
+
+// New Designer Route
+router.get('/', (req, res) => {
+    res.render('designers/new')
+})
+
+//Create Designer Route
+router.post('/', (req, res) => {
+    res.send('Create ')
+})
+module.exports = router
